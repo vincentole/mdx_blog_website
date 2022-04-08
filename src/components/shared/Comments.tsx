17ba3be1bb54a1fbe27503e545baf7edd7e18bc2
vincentole <63076449+vincentole@users.noreply.github.com>
2022-04-08 15:27:08 +0200

    added link to ISR

--- src/components/shared/Comments.tsx
+++ src/components/shared/Comments.tsx
@@ -5,8 +5,14 @@ export default function Comments() {
         <section className='article'>
             <div className='max-w-prose mx-auto'>
                 <p className='pb-4'>
-                    This blog uses <span className='italic'>Incremental Static Regeneration</span>.
-                    Therefore, comments or reactions may not be visible immediately.
+                    This blog uses{' '}
+                    <a
+                        href='https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration'
+                        className='italic'
+                    >
+                        Incremental Static Regeneration
+                    </a>
+                    . Therefore, comments or reactions may not be visible immediately.
                 </p>
                 <Giscus
                     repo='vincentole/personal_website'
